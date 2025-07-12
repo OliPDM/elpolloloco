bebac52a2f47140dffa7c15253589f3f35db092f
Oliver Geschine <oli.geschine@web.de>
2025-07-12 17:15:14 +0200

    add isHurt & isDead function + images

--- models/world.class.js
+++ models/world.class.js
@@ -28,7 +28,8 @@ class World {
         setInterval(() => {
             this.level.enemies.forEach((enemy) => {
                 if (this.character.isColliding(enemy)) {
-                    console.log('Collision with Character', enemy);
+                    this.character.hit();
+                    console.log('Collision with Character', 'enemy', this.character.energy);
                 }
             })
         }, 1000);
